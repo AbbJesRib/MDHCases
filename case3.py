cac55612c75fd417f8520db78c309c988e632ce9
AbbJesRib <54663096+AbbJesRib@users.noreply.github.com>
2020-10-12 16:09:32 +0200

    Update case3.py

--- case3.py
+++ case3.py
@@ -1,20 +1,69 @@
 import requests
 import os
-import sys
 
 url = "https://5hyqtreww2.execute-api.eu-north-1.amazonaws.com/artists/"
 r = requests.get(url)
 celebrityDictionary = r.json()
 
-line = "-----------------------------------"
+line = "-----------------------------------\n"
+
+artists = [i["name"] for i in celebrityDictionary["artists"]]
+
+
+def errorproof():
+    try:
+        input("Press enter to continue.")
+    except KeyboardInterrupt:
+        pass
 
 
 def printartists():
+    print(line)
     for i in celebrityDictionary["artists"]:
         print("|", i["name"])
+    print(line)
+    errorproof()
+
+
+def viewartist():
+    artistname = ""
+    while artistname.title() not in artists:
+        try:
+            artistname = input("Artist name > ")
+        except KeyboardInterrupt:
+            print("\nInvalid input")
+    id = ""
+    r = requests.get(url)
+    celebrityDictionary = r.json()
+    for i in celebrityDictionary["artists"]:
+        if i["name"] == artistname.title():
+            id = i["id"]
+            break
+    fetchedurl = url+id
+    p = requests.get(fetchedurl)
+    artistinfo = p.json()["artist"]
+    print(line+"\n"+artistinfo["name"]+"\n"+line)
+    for key, val in artistinfo.items():
+        if key != "name":
+            print(key.title()+"")
+            for i in val:
+                print("-", i)
+    print(line)
+    errorproof()
 
 
 instructions = {
-    "l": printartists
+    "List": printartists,
+    "View": viewartist,
+    "Exit": exit,
 }
-def clear(): os.system('cls')
+
+while True:
+    errormessage = ""
+    os.system("cls")
+    print(line+"List | List artists\nView | View artist profile\nExit | Exit application\n"+errormessage+line)
+    try:
+        whattodo = input(">> ").title()
+        instructions[whattodo]()
+    except (KeyError, KeyboardInterrupt, ValueError):
+        errormessage = line+"\nError: Invalid input. Try again\n"
